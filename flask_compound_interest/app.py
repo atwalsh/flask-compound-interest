9225f0c29cc38407ec39bc68d6868609f4076681
Adam Walsh <adamtwalsh@gmail.com>
2018-06-10 20:43:34 -0400

    add function

--- flask_compound_interest/app.py
+++ flask_compound_interest/app.py
@@ -8,15 +8,28 @@ app = Flask(__name__)
 def form():
     result = None
     if request.method == 'POST':
-        # Get the result of the compound interest function
-        pass
+        result = calculate_compound_interest(
+            p=int(request.form['principal']),
+            i=float(request.form['interest']),
+            t=int(request.form['times_per_year']),
+            n=int(request.form['years'])
+        )
     # Pass the result to the front end
-    return render_template('form.html')
+    return render_template('form.html', result=result)
 
 
-#
-# Write a compound interest function.
-#
+def calculate_compound_interest(p: int, i: float, t: int, n: int):
+    """
+    Calculate compound interest.
+
+    :param p: The principal investment amount.
+    :param i: The annual interest rate (decimal form).
+    :param t: Number of years the money is invested for.
+    :param n: Number of times that interest is compounded per year.
+    :return: The future value of the investment.
+    """
+    return p * ((1 + ((i / 100) / t)) ** (n * t))
+
 
 # Run the Flask app
 if __name__ == '__main__':
